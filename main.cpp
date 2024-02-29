d7af20a31c7144d1be8f9f868a778f7203fdd0e6
M-hypnos <46706215+M-hypnos@users.noreply.github.com>
2024-02-29 20:38:53 +0800

    test函数

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
-#include "utils/Any.hpp"
-#include "utils/SharePtr.hpp"
-#include "utils/UniquePtr.hpp"
+// utils.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
 #include <iostream>
+//#include <vld.h>
+#include <iostream>
+#include "utils/Any.h"
+#include "utils/SmartPtr.h"
 #include <string>
+#include <assert.h>
 #include <vector>
 
 using namespace selfUtils;
 using namespace std;
 
 struct Object {
-     Object(string s) : name_(s)
-        {
-            printf("object ctor: %s \n", name_.c_str());
-        }
+    Object(string s) : name_(s)
+    {
+        printf("object ctor: %s \n", name_.c_str());
+    }
     Object(int id, string name)
         : id_(id), name_(move(name))
-        {
-        }
+    {
+    }
     ~Object()
-        {
-            printf("object delete: %s \n", name_.c_str());
-        }
+    {
+        printf("object delete: %s \n", name_.c_str());
+    }
     int id_{};
     string name_;
 };
@@ -47,7 +52,7 @@ struct Doo
     Doo(const Doo&) { cout << "Doo copy ctor\n"; }
     Doo(Doo&&) { cout << "Doo move ctor\n"; }
     ~Doo() { cout << "~Doo dtor\n"; }
-    Doo(int v): val(v){ cout << "Doo ctor\n"; }
+    Doo(int v) : val(v) { cout << "Doo ctor\n"; }
     int val;
 };
 
@@ -57,8 +62,8 @@ void legacy_api(Doo* owning_foo)
     delete owning_foo;
 }
 
-void AnyTest(){
-Any n;
+void AnyTest() {
+    Any n;
     n = 2.2;
     printf("=====>>  %f\n", any_cast<double>(n));
     n = 1;
@@ -84,7 +89,7 @@ Any n;
     printf("====>>  type %s\n", a.getType().name());
     printf("=====>>  %s\n", any_cast<char const*>(a));
 
-    Any k = vector<int>{1,2,3};
+    Any k = vector<int>{ 1,2,3 };
     Any v;
     v = move(k);
     if (k.isNull()) {
@@ -149,7 +154,7 @@ void SharedPtrTest() {
     printf("moveAPtr use count %d \n", moveAPtr.use_count());
     printf("p use count %d \n", p.use_count());
     printf("ptr2 use count %d \n", ptr2.use_count());
-//------------------------------------------------
+    //------------------------------------------------
     cout << "1) unique ownership\n";
     {
         SharedPtr<Foo> sptr = new Foo(100);
@@ -197,7 +202,7 @@ void SharedPtrTest() {
             << ", sptr3 @ " << sptr3.get() << '\n'
             << "Leaving the scope...\n";
     }
-//------------------------------------------------
+    //------------------------------------------------
     SharedPtr<Foo> p1 = new Foo(100);
     SharedPtr<Foo> p2 = new Foo(200);
     auto print = [&]()
@@ -217,8 +222,8 @@ void SharedPtrTest() {
     print();
 }
 
-void UniquePtrTest(){
-     cout << "Example constructor(1)...\n";
+void UniquePtrTest() {
+    cout << "Example constructor(1)...\n";
     UniquePtr<Doo> up1a;
     UniquePtr<Doo> up1b(nullptr);
     cout << "Example constructor(2)...\n";
@@ -258,11 +263,152 @@ void UniquePtrTest(){
     cout << "up1->val:" << up1->val << "  up2->val:" << up2->val << '\n';
     up1.swap(up2);
     cout << "up1->val:" << up1->val << "  up2->val:" << up2->val << '\n';
+
+    UniquePtr<Doo> upA(new Doo);
+    UniquePtr<Doo> upB(new Doo);
+    upB = move(upA);
 }
 
-int main(){
+WeakPtr<int> gwp;
+
+void observe_gwp()
+{
+    cout << "use_count(): " << gwp.use_count() << "\t id: ";
+    if (auto sp = gwp.lock())
+        std::cout << *sp << '\n';
+    else
+        std::cout << "??\n";
+}
+
+void share_recursively(SharedPtr<int> sp, int depth)
+{
+    observe_gwp();
+    if (1 < depth)
+        share_recursively(sp, depth - 1);
+    observe_gwp();
+}
+
+WeakPtr<int> gw;
+void f()
+{
+    if (!gw.expired())
+        cout << "gw is valid\n";
+    else
+        cout << "gw is expired\n";
+}
+
+void observe(WeakPtr<int> weak)
+{
+    if (auto p = weak.lock())
+        cout << "\tobserve() is able to lock weak_ptr<>, value=" << *p << '\n';
+    else
+        cout << "\tobserve() is unable to lock weak_ptr<>\n";
+}
+
+void WeakPtrTest() {
+    WeakPtr<Foo> w_ptr;
+    {
+        auto ptr = SharedPtr<Foo>(new Foo);
+        w_ptr = ptr;
+        cout << "w_ptr.use_count() inside scope: " << w_ptr.use_count() << '\n';
+    }
+    cout << "w_ptr.use_count() out of scope: " << w_ptr.use_count() << '\n';
+    cout << "w_ptr.expired() out of scope: " << boolalpha << w_ptr.expired() << '\n';
+    cout<< '\n';
+    //---------------
+    auto shared = SharedPtr<int>(new int), shared2 = shared, shared3 = shared2;
+    auto weak = WeakPtr<int>{ shared };
+    cout << boolalpha
+        << "shared.use_count(): " << shared.use_count() << '\n'
+        << "weak.use_count(): " << weak.use_count() << '\n'
+        << "weak.expired(): " << weak.expired() << '\n';
+
+    weak.reset();
+    cout << "weak.reset();\n"
+        << "shared.use_count(): " << shared.use_count() << '\n'
+        << "weak.use_count(): " << weak.use_count() << '\n'
+        << "weak.expired(): " << weak.expired() << '\n';
+    cout << '\n';
+    //---------------
+    SharedPtr<Foo> sp1 = SharedPtr<Foo>(new Foo(100));
+    SharedPtr<Foo> sp2 = SharedPtr<Foo>(new Foo(200));
+    WeakPtr<Foo> wp1 = sp1;
+    WeakPtr<Foo> wp2 = sp2;
+    auto print = [&]()
+    {
+        auto p1 = wp1.lock();
+        auto p2 = wp2.lock();
+        cout << " p1=" << (p1 ? p1->print() : "nullptr");
+        cout << " p2=" << (p2 ? p2->print() : "nullptr") << '\n';
+    };
+    print();
+    wp1.swap(wp2);
+    print();
+    wp1.reset();
+    print();
+    wp1.swap(wp2);
+    print();
+    cout << '\n';
+    //---------------
+    observe_gwp();
+    {
+        auto sp = SharedPtr<int>(new int(42));
+        gwp = sp;
+        observe_gwp();
+        share_recursively(sp, 3);
+        observe_gwp();
+    }
+    observe_gwp();
+    cout << '\n';
+    //---------------
+    {
+        auto sp = SharedPtr<int>(new int(42));
+        gw = sp;
+        f();
+    }
+    f();
+    cout << '\n';
+    //---------------
+    WeakPtr<int> weakP;
+    cout << "weak_ptr<> is not yet initialized\n";
+    observe(weakP);
+    {
+        auto shared = SharedPtr<int>(new int(42));
+        weakP = shared;
+        cout << "weak_ptr<> is initialized with shared_ptr\n";
+        observe(weakP);
+    }
+    cout << "shared_ptr<> has been destructed due to scope exit\n";
+    observe(weakP);
+}
+
+void SmartPtrTest() {
+    cout << "-----" << '\n';
+    SharedPtr<int> sp1(new int(10));
+    WeakPtr<int> wp1 = sp1;
+    cout << sp1.use_count() << '\n';
+    wp1.lock();
+    cout << sp1.use_count() << '\n';
+    SharedPtr<int> sp2 = wp1.lock();
+    cout << sp1.use_count() << '\n';
+    cout << sp2.use_count() << '\n';
+    cout << "-----" << '\n';
+    SharedPtr<int> sptr1;
+    cout << sptr1.use_count() << '\n';
+    SharedPtr<int> sptr2 = sptr1;
+    cout << sptr2.use_count() << '\n';
+    SharedPtr<int> sptr3(new int(10));
+    sptr1 = sptr3;
+    cout << sptr1.use_count() << '\n';
+    cout << sptr2.use_count() << '\n';
+    cout << sptr3.use_count() << '\n';
+}
+
+int main() {
     AnyTest();
     SharedPtrTest();
     UniquePtrTest();
+    WeakPtrTest();
+    SmartPtrTest();
     return 0;
 }

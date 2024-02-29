41ef173bea3f69ee27afc92fccd452c66dac2162
M-hypnos <46706215+M-hypnos@users.noreply.github.com>
2024-02-29 11:31:48 +0800

    SharedPtr

--- main.cpp
+++ main.cpp
@@ -7,17 +7,40 @@ using namespace selfUtils;
 using namespace std;
 
 struct Object {
-    Object() = default;
+     Object(string s) : name_(s)
+        {
+            printf("object ctor: %s \n", name_.c_str());
+        }
     Object(int id, string name)
         : id_(id), name_(move(name))
-    {
-    }
+        {
+        }
+    ~Object()
+        {
+            printf("object delete: %s \n", name_.c_str());
+        }
     int id_{};
     string name_;
 };
 
-int main(){
-    Any n;
+struct Foo
+    {
+        Foo(int n = 0) noexcept : bar(n)
+        {
+            cout << "Foo::Foo(), bar = " << bar << " @ " << this << '\n';
+        }
+        ~Foo()
+        {
+            cout << "Foo::~Foo(), bar = " << bar << " @ " << this << '\n';
+        }
+        int getBar() const noexcept { return bar; }
+        string print() const { return to_string(bar); }
+    private:
+        int bar;
+    };
+
+void AnyTest(){
+Any n;
     n = 2.2;
     printf("=====>>  %f\n", any_cast<double>(n));
     n = 1;
@@ -69,5 +92,115 @@ int main(){
     q = 4;
     Any oo = any_cast<int>(vv) + any_cast<int>(q);
     printf("%d", any_cast<int>(oo));
+}
+
+void SharedPtrTest() {
+    SharedPtr<Object> emptyPtr;
+    printf("emptyPtr use count %d \n", emptyPtr.use_count());
+    SharedPtr<Object> ptr(new Object("a"));
+    printf("ptr use count %d \n", ptr.use_count());
+
+    SharedPtr<Object> copyPtr(ptr);
+    printf("ptr use count %d \n", ptr.use_count());
+    printf("copyPtr use count %d \n", copyPtr.use_count());
+
+    SharedPtr<Object> ptr1(new Object("b"));
+    ptr1 = ptr1;
+    printf("ptr1 use count %d \n", ptr1.use_count());
+    SharedPtr<Object> copyPtr1(ptr1);
+    ptr1 = ptr;
+    printf("ptr use count %d \n", ptr.use_count());
+    printf("ptr1 use count %d \n", ptr1.use_count());
+    printf("copyPtr1 use count %d \n", copyPtr1.use_count());
+
+    SharedPtr<Object> movePtr(move(ptr));
+    printf("ptr use count %d \n", ptr.use_count());
+    printf("movePtr use count %d \n", movePtr.use_count());
+
+    SharedPtr<Object> p = ptr1;
+    printf("ptr1 use count %d \n", ptr1.use_count());
+    printf("p use count %d \n", p.use_count());
+
+    SharedPtr<Object> moveAPtr(new Object("c"));
+    SharedPtr<Object> ptr2 = moveAPtr;
+    printf("ptr2 use count %d \n", ptr2.use_count());
+    printf("moveAPtr use count %d \n", moveAPtr.use_count());
+
+    moveAPtr = std::move(ptr1);
+    printf("ptr1 use count %d \n", ptr1.use_count());
+    printf("moveAPtr use count %d \n", moveAPtr.use_count());
+    printf("p use count %d \n", p.use_count());
+    printf("ptr2 use count %d \n", ptr2.use_count());
+//------------------------------------------------
+    cout << "1) unique ownership\n";
+    {
+        SharedPtr<Foo> sptr = new Foo(100);
+
+        cout << "Foo::bar = " << sptr->getBar() << ", use_count() = "
+            << sptr.use_count() << '\n';
+
+        cout << "call sptr.reset()...\n";
+        sptr.reset();
+        cout << "After reset(): use_count() = " << sptr.use_count()
+            << ", sptr = " << sptr.get() << '\n';
+    }
+
+    cout << "\n2) unique ownership\n";
+    {
+        SharedPtr<Foo> sptr = new Foo(200);
+
+        cout << "Foo::bar = " << sptr->getBar() << ", use_count() = "
+            << sptr.use_count() << '\n';
+
+        cout << "call sptr.reset()...\n";
+        sptr.reset(new Foo{ 222 });
+        cout << "After reset(): use_count() = " << sptr.use_count()
+            << ", sptr = " << sptr.get() << "\nLeaving the scope...\n";
+    }
+
+    cout << "\n3) multiple ownership\n";
+    {
+        SharedPtr<Foo> sptr1 = new Foo(300);
+        SharedPtr<Foo> sptr2 = sptr1;
+        SharedPtr<Foo> sptr3 = sptr2;
+
+        cout << "Foo::bar = " << sptr1->getBar() << ", use_count() = "
+            << sptr1.use_count() << '\n';
+
+        cout << "call sptr1.reset()...\n";
+        sptr1.reset(new Foo{ 333 });
+
+        cout << "After reset():\n"
+            << "sptr1.use_count() = " << sptr1.use_count()
+            << ", sptr1 @ " << sptr1.get() << '\n'
+            << "sptr2.use_count() = " << sptr2.use_count()
+            << ", sptr2 @ " << sptr2.get() << '\n'
+            << "sptr3.use_count() = " << sptr3.use_count()
+            << ", sptr3 @ " << sptr3.get() << '\n'
+            << "Leaving the scope...\n";
+    }
+//------------------------------------------------
+    SharedPtr<Foo> p1 = new Foo(100);
+    SharedPtr<Foo> p2 = new Foo(200);
+    auto print = [&]()
+    {
+        cout << " p1=" << (p1.get() ? p1->print() : "nullptr");
+        cout << " p2=" << (p2.get() ? p2->print() : "nullptr") << '\n';
+    };
+    print();
+
+    p1.swap(p2);
+    print();
+
+    p1.reset();
+    print();
+
+    p1.swap(p2);
+    print();
+}
+
+int main(){
+    AnyTest();
+    SharedPtrTest();
     return 0;
 }
